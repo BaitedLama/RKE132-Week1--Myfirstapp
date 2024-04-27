3ac789f46aa97b4c59d38a6ca23695e41e995419
BaitedLama <you@example.comkaspar.kool.kask@gmail.com>
2024-04-27 11:30:26 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nimi
+//rakendus tervitab kasutajat nimepidi
+using System.Security.Cryptography.X509Certificates;
+
+Console.WriteLine("Enter your name:");
+//string-sõne
+string userName = userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+Console.WriteLine($"Hello, {userName} !");
